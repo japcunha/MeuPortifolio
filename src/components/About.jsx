f7c81bea5d17853c10e969e1955747663a2e13f4
Joane <138986247+japcunha@users.noreply.github.com>
2025-07-04 16:31:16 -0300

    feat: add components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,25 +4,18 @@ export default function About() {
   return (
     <section id="aboutMe">
       <div className="container">
-        <div className="image">
-          <img src="img/cat.jpeg" alt="me" className="img" />
-        </div>
-        <div className="intro">
-          <h1 className="text-xl font-semibold mb-2">
-            Olá! me chamo Joane Cunha
-          </h1>
-          <p>
-            Sou estudante de Analise e Desenvolvimento de Sistemas, atualmente
-            no 4º período e estou em busca de uma oportunidade para crescer como
-            desenvolvedora web.{" "}
-          </p>
-          <p>
-            Tenho conhecimento em Lógica de programação, POO, e nas seguintes
-            linguagens e tecnologias:
-          </p>
-          <Skills />
-        </div>
-      </div>
+          <img src="imgs/cat.jpeg" alt="me" className="img" />
+        <h2 className="text-xl font-semibold mb-2">
+          Olá! me chamo Joane Cunha
+        </h2>
+        <p>
+          Sou estudante de [seu curso] e estou em busca de uma oportunidade para
+          crescer como desenvolvedora web. Tenho conhecimentos em HTML, CSS,
+          JavaScript e estou aprendendo React.
+        </p>
+        <div>tecnologias:</div>
+
+</div>
     </section>
   );
 }
