80aeef3789ff7f2b6a9a5a09734da359c27be935
Joane <138986247+japcunha@users.noreply.github.com>
2025-07-24 20:31:16 -0300

    css: melhorias e resposividade

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import '../style/skillBar.css';
+import "../style/skillBar.css";
+import { useEffect, useRef } from "react";
 
 const SkillBar = ({ skill, level }) => {
+  const progress = useRef(null);
 
-    return (
-        <div className="skill">
-            <span>{skill}</span>
-            <div className="progress-bar">
-                <div className="progress" style={{ width: `${level}%`}}>
-                </div>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    if (progress.current) {
+      progress.current.style.width = "0%";
+      setTimeout(() => {
+        progress.current.style.width = `${level}%`;
+      }, 100);
+    }
+  }, [level]);
+
+  return (
+    <div className="skill">
+      <span>{skill}</span>
+      <div className="progress-bar">
+        <div className="progress" ref={progress}></div>
+      </div>
+    </div>
+  );
 };
 export default SkillBar;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import SkillBar from "./SkillBar.jsx";
 
 const Skills = () => {
